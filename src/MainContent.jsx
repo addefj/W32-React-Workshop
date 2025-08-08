24efb1ace51bbac16de1313ccc7d35da55409547
addefj <adde.fj@gmail.com>
2025-08-08 12:03:13 +0200

    added dynamic sidebar for smaller screens

--- src/MainContent.jsx
+++ src/MainContent.jsx
@@ -2,12 +2,15 @@ import React from 'react'
 import Header from './Header'
 import Form from './Form'
 import Card from './Card'
+import SidebarHorizontal from './SidebarHorizontal';
+
 
 const MainContent = () => {
   return (
     
-          <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4 offset-md-2">
+          <main className="col px-md-4 offset-lg-2">
             <Header />
+               <SidebarHorizontal /> {/* 👈 Place horizontal icon bar here for md and below */}
             <Form />
             <Card />
           </main>
